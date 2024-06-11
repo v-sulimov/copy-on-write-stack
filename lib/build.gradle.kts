7c07749ef7e0b3e44dd5e2ad2bd5344faaa96491
Vitaly Sulimov <v.sulimov@fastmail.fm>
2024-06-12 01:58:27 +0300

    Create Gradle project.
    This project was created using Gradle init task.
    Project configuration:
    Gradle version: 8.8
    Type of build: Library
    Implementation language: Kotlin
    Target Java version: 21
    Project name: copy-on-write-stack
    Build script DSL: Kotlin
    Test framework: kotlin.test
    Using new API and behaviour: no

---
+++ lib/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    alias(libs.plugins.jvm)
+
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use the Kotlin JUnit 5 integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // Use the JUnit 5 integration.
+    testImplementation(libs.junit.jupiter.engine)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
